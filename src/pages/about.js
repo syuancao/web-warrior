4a8132af2f306bf94629e4c7e258b931e820fb67
syuancao <73942181+syuancao@users.noreply.github.com>
2021-04-17 05:24:14 +0800

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos dolor
+          laborum culpa impedit rem, sed quo alias harum possimus voluptatum
+          odio ad provident magnam minima porro esse autem at dolorem!
+        </p>
+      </div>
+    </Layout>
+  )
+}
---
+++ src/pages/projects/index.js
@@ -0,0 +1,65 @@
+import { graphql, Link } from "gatsby"
+import React from "react"
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
+import Img from "gatsby-image"
+
+export default function Projects({ data }) {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Protfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(projects => (
+            <Link
+              to={"/projects/" + projects.frontmatter.slug}
+              key={projects.id}
+            >
+              <div>
+                <Img fluid={projects.frontmatter.thumb.childImageSharp.fluid} />
+                <h3>{projects.frontmatter.title}</h3>
+                <p>{projects.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <p>Like what you see? Email me at {contact} for a quote!</p>
+      </div>
+    </Layout>
+  )
+}
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    projects: allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`
